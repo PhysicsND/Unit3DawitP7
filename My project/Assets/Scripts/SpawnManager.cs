59dea0a163bb02426eeb6d58cf944d9237d5885e
PhysicsND <nayakdawit@outlook.com>
2026-03-24 15:24:14 -0700

    3.1 11 3.2 1-4

--- My project/Assets/Scripts/SpawnManager.cs
+++ My project/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,22 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 SpawnPos = new Vector3(25,0,0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(obstaclePrefab, SpawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle ()
+    {
+        Instantiate(obstaclePrefab, SpawnPos, obstaclePrefab.transform.rotation);
     }
 }
